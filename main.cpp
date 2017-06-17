4b60966cf041f6aa22373f5e9329ff70eddc2e9b
stuffjunkgames <msquared.169@gmail.com>
2017-06-17 19:02:41 -0400

    basic classes

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <SFML/Window.hpp>
 #include <cstdlib>
 #include <iostream>
+#include <list>
 
 #define GAME_TOP 24
 #define GAME_LEFT 12
@@ -14,85 +15,207 @@
 #define BLOCK_SIZE 20
 #define MAX_LENGTH 100
 
-class Player
-{
-    int playerNumber;
-    std::string name;
-    sf::Color color;
-    std::list<Territory> territories; // territories owned by this player
+class Player;
+class Territory;
 
-    public Player(int number, std::string name, sf::Color color)
-    {
-        playerNumber = number;
-        this.name = name;
-        this.color = color;
-    }
-
-    void CaptureTerritory(Territory captured)
-    {
-        captured.ChangeOwner(this);
-        territories.push_back(captured);
-    }
-
-    void LostTerritory(Territory captured)
-    {
-        territories.remove(captured);
-    }
-};
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Class prototypes
 
 // capturable territory in game world
 class Territory
 {
-    sf::Shape territory;
+    int id;
+    sf::ConvexShape territory;
+    sf::Text armyDisplay;
     std::string name;
-    Player owner;
+    Player *owner;
     unsigned int army;  // how many armies player has in territory
     std::vector<Territory> connected;
 
-    public Territory(sf::Shape shape, std::string name, Player player, unsigned int army)
+public:
+    Territory(int id, sf::ConvexShape *shape, std::string name, Player *player, unsigned int army);
+    void addConnection(Territory connection);
+    void ChangeOwner(Player *newOwner, unsigned int newArmy);
+    sf::ConvexShape getShape()
     {
-        territory = shape;
-        this.name = name;
-        owner = player;
-        this.army = army;
+        return territory;
     }
 
-    void addConnection(Territory connection)
+    bool operator == (const Territory &other)
     {
-        connected.push_back(connection);
+        return this->id == other.id;
     }
 
-    void ChangeOwner(Player newOwner)
+    // find centroid of territory
+    // move armyDisplay to centroid of territory
+    // set armyDisplay number
+    // add function for adding/subtracting armies
+
+}; // Territory
+
+class Player
+{
+    int playerNumber;
+    std::string name;
+    std::list<Territory> territories; // territories owned by this player
+
+public:
+    sf::Color color;
+
+    Player(int number, std::string name, sf::Color color);
+    void CaptureTerritory(Territory *captured, unsigned int army);
+    void LostTerritory(Territory *captured);
+
+    bool operator == (const Player &other)
     {
-        owner = newOwner;
+        return this->playerNumber == other.playerNumber;
     }
-};
+
+}; // Player
+
+// Class prototypes ^
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Territory class definitions
+
+Territory::Territory(int id, sf::ConvexShape *shape, std::string name, Player *player, unsigned int army)
+{
+    this->id = id;
+    territory = *shape;
+    this->name = name;
+    owner = player;
+    this->army = army;
+    territory.setFillColor(owner->color);
+}
+
+void Territory::addConnection(Territory connection)
+{
+    connected.push_back(connection);
+}
+
+void Territory::ChangeOwner(Player *newOwner, unsigned int newArmy)
+{
+    owner = newOwner;
+    army = newArmy;
+    territory.setFillColor(owner->color);
+}
+
+// Territory class ^
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Player class definitions
+
+Player::Player(int number, std::string name, sf::Color color)
+{
+    playerNumber = number;
+    this->name = name;
+    this->color = color;
+}
+
+
+void Player::CaptureTerritory(Territory *captured, unsigned int army)
+{
+    captured->ChangeOwner(this, army);
+    territories.push_back(*captured);
+}
+
+void Player::LostTerritory(Territory *captured)
+{
+    territories.remove(*captured);
+}
+
+// Player class definitions ^
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// World class
 
 // collection of territories that make up the game world
 class World
 {
-    std::vector<Territory> territories;
+    std::vector<Territory> territoryList;
+    std::vector<Player> playerList;
 
-    public World()
+public:
+    World()
     {
         // make the world.
-        // TODO: read in from a file (JSON?)
+        // TODO: read in from a file (JSON?) or database (SQL?)
 
+        playerList.push_back(*(new Player(1, "Player 1", sf::Color::Blue)));
+        playerList.push_back(*(new Player(2, "Player 2", sf::Color::Red)));
+
+        sf::ConvexShape shape(4);
+        shape.setPointCount(4);
+
+        shape.setPoint(0, sf::Vector2f(0,0));
+        shape.setPoint(1, sf::Vector2f(0,100));
+        shape.setPoint(2, sf::Vector2f(100,100));
+        shape.setPoint(3, sf::Vector2f(100,0));
+
+        territoryList.push_back(*(new Territory(1, &shape, "Territory 1", &playerList[0], 12)));
+
+        shape.setPoint(0, sf::Vector2f(100,0));
+        shape.setPoint(1, sf::Vector2f(100,100));
+        shape.setPoint(2, sf::Vector2f(200,100));
+        shape.setPoint(3, sf::Vector2f(200,0));
+
+        territoryList.push_back(*(new Territory(1, &shape, "Territory 2", &playerList[1], 5)));
+
+        shape.setPoint(0, sf::Vector2f(0,100));
+        shape.setPoint(1, sf::Vector2f(0,200));
+        shape.setPoint(2, sf::Vector2f(100,200));
+        shape.setPoint(3, sf::Vector2f(100,100));
+
+        territoryList.push_back(*(new Territory(1, &shape, "Territory 3", &playerList[1], 10)));
+
+        shape.setPoint(0, sf::Vector2f(100,100));
+        shape.setPoint(1, sf::Vector2f(100,200));
+        shape.setPoint(2, sf::Vector2f(200,200));
+        shape.setPoint(3, sf::Vector2f(200,100));
+
+        territoryList.push_back(*(new Territory(1, &shape, "Territory 4", &playerList[0], 20)));
 
     }
-};
+
+    Territory getTerritory(int index)
+    {
+        return territoryList.at(index);
+    }
+
+    unsigned int territoryNumber()
+    {
+         return territoryList.size();
+    }
+}; // World
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// main
 
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1024, 768), "Risk");
     sf::RectangleShape background = sf::RectangleShape(sf::Vector2<float>(GAME_WIDTH, GAME_HEIGHT));
-    sf::RectangleShape snake[MAX_LENGTH];
     sf::RectangleShape target = sf::RectangleShape(sf::Vector2<float>(BLOCK_SIZE, BLOCK_SIZE));
 
     background.setPosition(GAME_LEFT, GAME_TOP);
-    background.setFillColor(sf::Color::Blue);
+    background.setFillColor(sf::Color::Black);
 
+    World world = *(new World());
 
+    sf::ConvexShape convex;
+
+    convex.setPointCount(5);
+
+    convex.setPoint(0, sf::Vector2f(0, 0));
+    convex.setPoint(1, sf::Vector2f(150, 10));
+    convex.setPoint(2, sf::Vector2f(120, 90));
+    convex.setPoint(3, sf::Vector2f(30, 100));
+    convex.setPoint(4, sf::Vector2f(0, 50));
 
     std::srand(std::time(0));
 
@@ -125,6 +248,14 @@ int main()
             // draw
             window.clear();
             window.draw(background);
+            window.draw(convex);
+            for(unsigned int i = 0; i < world.territoryNumber(); i++)
+            {
+                Territory tmp = world.getTerritory(i);
+                sf::ConvexShape s = tmp.getShape();
+                window.draw(world.getTerritory(i).getShape());
+                // draw text for armies
+            }
             window.display();
         }
 
@@ -132,4 +263,6 @@ int main()
     }
 
     return 0;
-}
+} // main
+
+//////////////////////////////////////////////////////////////////////////////////////////
